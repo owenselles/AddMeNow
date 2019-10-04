7700f192812fe7800859a4d23c25f1447379e812
Owen Selles <owensellesyt@gmail.com>
2019-10-04 14:09:40 +0200

    add admob and buttons, emoji

--- app/src/main/java/com/owenselles/addmenow/UserAdapter.kt
+++ app/src/main/java/com/owenselles/addmenow/UserAdapter.kt
@@ -1,22 +1,24 @@
+/*
+ * Copyright (c) 2019 Owen Selles
+ * All rights reserved.
+ */
+
 package com.owenselles.addmenow
 
-import android.content.Context
-import android.graphics.Color
 import android.graphics.Color.*
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_item.view.*
-import androidx.cardview.widget.CardView
-
-
 
 class UsersAdapter(val users: List<User>) : RecyclerView.Adapter<UsersAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
-        return ViewHolder(view)    }
+        return ViewHolder(view)
+    }
 
     override fun getItemCount() = users.size
 
@@ -25,9 +27,21 @@ class UsersAdapter(val users: List<User>) : RecyclerView.Adapter<UsersAdapter.Vi
         holder.snapName.text = users[position].snapName
         holder.age.text = users[position].age.plus("yo")
         if (users[position].gender.equals("male")) {
-            holder.gender.text = "\uD83D\uDC66"
+            holder.gender.text = """👦"""
         } else {
-            holder.gender.text = "\uD83D\uDC67"
+            holder.gender.text = """👧"""
+        }
+        if (users[position].looking.equals("streak")) {
+            holder.looking.text = """🔥"""
+        } else if (users[position].looking.equals("friend")) {
+            holder.looking.text = """🎉"""
+        } else if (users[position].looking.equals("friend+")) {
+            holder.looking.text = """🍑"""
+        } else {
+            holder.looking.text = """👀"""
+        }
+        holder.button.setOnClickListener() {
+            //TODO open add screen
         }
     }
 
@@ -35,6 +49,8 @@ class UsersAdapter(val users: List<User>) : RecyclerView.Adapter<UsersAdapter.Vi
         val snapName: TextView = view.list_username
         val age: TextView = view.list_age
         val gender: TextView = view.textView3
+        val looking: TextView = view.emoji
+        val button: Button = view.button
     }
 }
 
